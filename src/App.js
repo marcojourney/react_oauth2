65a215bc0d1ecf837ad4f41221ffb5d39cd28a58
Marco <mornsophannamis@gmail.com>
2023-08-28 11:18:56 +0700

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const baseUrl = 'http://127.0.0.1:3000';
+const appId = 'kiripostapp';
+const redirectUri = 'http://localhost:3000/callback';
+
+function Modal({ isOpen, onClose, onConfirm }) {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <p>Do you want to proceed with the authorization?</p>
+        <button onClick={onClose}>Cancel</button>
+        <button onClick={onConfirm}>Confirm</button>
+      </div>
+    </div>
+  );
+}
+
 function App() {
+  const [authorizationCode, setAuthorizationCode] = useState(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleAuthorization = () => {
+    fetch(`${baseUrl}/oauth2/authorize?appId=${appId}&scope=&redirectUri=${redirectUri}`)
+    .then(resposne => resposne.json())
+    .then(resposne => {
+      // Create a URL object from the redirectUrl
+      const url = new URL(resposne.redirectUrl);
+
+      // Get the value of the 'code' parameter from the query string
+      const code = url.searchParams.get('code');
+      setAuthorizationCode(code);
+    });
+  }
+
+  const handleGetAccessToken = () => {
+    fetch(`${baseUrl}/oauth2/token?authorizationCode=${authorizationCode}`)
+    .then(resposne => resposne.json())
+    .then(resposne => {
+      const { accessToken, refreshToken } = resposne;
+      console.log("Access Token:", accessToken);
+      console.log("Refresh Token:", refreshToken);
+    });
+  }
+
+  const handleClose = () => {
+    setIsVisible(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!authorizationCode && <button onClick={handleAuthorization}>Authorization</button>}
+      {authorizationCode && <button onClick={handleGetAccessToken}>Get Access Token</button>}
+
+      <Modal isOpen={isVisible} onClose={handleClose} />
     </div>
   );
 }
