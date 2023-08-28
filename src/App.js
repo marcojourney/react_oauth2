379d816561362955ef541dda5819d1b8e75c981a
Marco <mornsophannamis@gmail.com>
2023-08-28 13:13:15 +0700

    Some modification

--- src/App.js
+++ src/App.js
@@ -5,32 +5,17 @@ const baseUrl = 'http://127.0.0.1:3000';
 const appId = 'kiripostapp';
 const redirectUri = 'http://localhost:3000/callback';
 
-function Modal({ isOpen, onClose, onConfirm }) {
-  if (!isOpen) return null;
-
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <p>Do you want to proceed with the authorization?</p>
-        <button onClick={onClose}>Cancel</button>
-        <button onClick={onConfirm}>Confirm</button>
-      </div>
-    </div>
-  );
-}
-
 function App() {
   const [authorizationCode, setAuthorizationCode] = useState(null);
-  const [isVisible, setIsVisible] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [accessToken, setAccessToken] = useState(null);
 
   const handleAuthorization = () => {
     fetch(`${baseUrl}/oauth2/authorize?appId=${appId}&scope=&redirectUri=${redirectUri}`)
     .then(resposne => resposne.json())
     .then(resposne => {
-      // Create a URL object from the redirectUrl
       const url = new URL(resposne.redirectUrl);
 
-      // Get the value of the 'code' parameter from the query string
       const code = url.searchParams.get('code');
       setAuthorizationCode(code);
     });
@@ -41,21 +26,31 @@ function App() {
     .then(resposne => resposne.json())
     .then(resposne => {
       const { accessToken, refreshToken } = resposne;
-      console.log("Access Token:", accessToken);
-      console.log("Refresh Token:", refreshToken);
+      setAccessToken(accessToken);
     });
   }
 
-  const handleClose = () => {
-    setIsVisible(false);
+  const fetchUsers = () => {
+    fetch(`${baseUrl}/users`, 
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    })
+    .then(resposne => resposne.json())
+    .then(resposne => {
+      if (resposne) {
+        setUsers(resposne);
+        console.log('Users:', resposne);
+      }
+    })
   }
 
   return (
     <div className="App">
       {!authorizationCode && <button onClick={handleAuthorization}>Authorization</button>}
       {authorizationCode && <button onClick={handleGetAccessToken}>Get Access Token</button>}
-
-      <Modal isOpen={isVisible} onClose={handleClose} />
+      <button onClick={fetchUsers}>Get Users</button>
     </div>
   );
 }
