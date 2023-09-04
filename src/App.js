cb3e19cb8d9fff65d121367b43355e3ef3492bb2
Marco <mornsophannamis@gmail.com>
2023-09-04 16:11:22 +0700

    some modification

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const redirectUri = 'http://localhost:3000/callback';
 
 function App() {
   // Set a cookie accessible across all subdomains of example.com
-  document.cookie = "mycookie=myvalue; domain=.vercel.app; path=/; secure; samesite=none";
+  document.cookie = "mycookie=myvalue; domain=vercel.app; path=/; secure; samesite=none";
 
   const [authorizationCode, setAuthorizationCode] = useState(null);
   const [users, setUsers] = useState([]);
